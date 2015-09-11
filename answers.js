75b5e00aabec60b418d960d056578e8f956b0707
Nathaniel Kitzke <nat.kitzke@gmail.com>
2015-09-11 17:58:10 +0000

    wrote lastChar function

--- answers.js
+++ answers.js
@@ -1,5 +1,11 @@
-//Takes in a string and outputs first character
+// 1 - Takes in a string and outputs first character
 function firstChar(string){
     return string[0];
 }
 console.log(firstChar("Ziad"));
+
+// 2 - Takes in a string and outputs last character
+function lastChar(string){
+        return string[string.length - 1];
+}
+console.log(lastChar("Nathaniel"));
